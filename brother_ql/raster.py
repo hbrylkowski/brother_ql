5ff2c29161bcf729167eb2fcb4631a9557d22c96
Alessio Gerace <a.gerace@inrim.it>
2021-11-18 18:34:22 +0100

    FIX driver for chain printing and feed paper

--- brother_ql/raster.py
+++ brother_ql/raster.py
@@ -273,9 +273,9 @@ class BrotherQLRaster(object):
 
     def add_print(self, last_page=True):
         if last_page:
-            self.data += b'\x1A'  # 0x1A = ^Z = SUB; here: EOF = End of File
             self.data += b'\x1B\x69\x43\x02'  # ESC i C n
             self.data += b'\x0C'
+            self.data += b'\x1A'  # 0x1A = ^Z = SUB; here: EOF = End of File
         else:
             self.data += b'\x0D\x0A'  # CR LF
 
